b222977d01fada83d411ee16acbd15e444031593
TJ Vinsavich <tjvinsavich@gmail.com>
2021-06-23 11:29:29 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the temperature converter!");
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            Console.WriteLine("1. Fahrenheit to Celsius");
+            Console.WriteLine("2. Celsius to Fahrenheit");
+
+            var userOption = int.Parse(Console.ReadLine());
+
+            if(userOption == 1)
+            {
+                Console.Write("Give a temperature in Fahrenheit: ");
+                var temp = double.Parse(Console.ReadLine());
+
+                var result = TempConverter.FahrenheitToCelsius(temp);
+
+                Console.WriteLine($"The new temperature is {result} degrees Celsius.");
+            }
+            else if (userOption == 2)
+            {
+                Console.Write("Give a temperature in Celsius: ");
+                var temp = double.Parse(Console.ReadLine());
+
+                var result = TempConverter.CelsiusToFahrenheit(temp);
+
+                Console.WriteLine($"The new temperature is {result} degrees Fahrenheit.");
+            }
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+
+        }
+
+        public static double FahrenheitToCelsius (double temp)
+        {
+            return (temp - 32) * (5.0 / 9.0);
+        }
+
+        public static double CelsiusToFahrenheit (double temp)
+        {
+            return (temp * 9.0 / 5.0) + 32;
+        }
+    }
+}
